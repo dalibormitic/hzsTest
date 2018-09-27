43ec6deb3c868501c7286ed19871514fd0230e3f
Katarina <kjerinic@users.noreply.github.com>
2018-09-27 23:57:12 +0200

    Add files via upload

--- js/init.js
+++ js/init.js
@@ -1,30 +1,26 @@
-
-    $(function () {
-
-        $('.sidenav')
-        .sidenav()
-        .on('click tap', 'li a', () => {
-            $('.sidenav').sidenav('close');
-        });
-
-        $(window).load(function() {
-            
+$(function () {
+    $('.sidenav').sidenav().on('click tap', 'li a', () => {
+        $('.sidenav').sidenav('close');
+    });
+    $(window).load(function() {
+        if ($(window).width() <= 600) {
+            $('.social').addClass('btn-small');
+        } else {
+            $('.social').removeClass('btn-small');
+        }
+        $(window).resize(function () {
             if ($(window).width() <= 600) {
                 $('.social').addClass('btn-small');
             } else {
                 $('.social').removeClass('btn-small');
             }
-            
-            $(window).resize(function () {
-            
-                if ($(window).width() <= 600) {
-                    $('.social').addClass('btn-small');
-                } else {
-                    $('.social').removeClass('btn-small');
-                }
-            });
         });
+    });
+    $('.scrollspy').scrollSpy();
+}); 
 
-    }); // end of document ready
+
+
+// end of document ready
 
 
