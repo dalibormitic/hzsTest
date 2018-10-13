ba2c47cdfb751f1655e3ba30d237856830bdb5bd
dalibormitic <dalibor.mitic997@gmail.com>
2018-10-13 15:14:13 +0200

    Sredjena prijava

--- js/init.js
+++ js/init.js
@@ -1,28 +1,29 @@
 $(function () {
-    $('.sidenav').sidenav().on('click tap', 'li a', () => {
-        $('.sidenav').sidenav('close');
-    });
-    $(window).load(function() {
-        if ($(window).width() <= 600) {
-            $('.social').addClass('btn-small');
-        } else {
-            $('.social').removeClass('btn-small');
-        }
-        $(window).resize(function () {
-            if ($(window).width() <= 600) {
-                $('.social').addClass('btn-small');
-            } else {
-                $('.social').removeClass('btn-small');
-            }
-        });
-    });
-    $('.scrollspy').scrollSpy();
-    $('.collapsible').collapsible();
-    $('.tooltipped').tooltip();
-    $('.tap-target').tapTarget();
 
-    // Vertical Timeline - by CodyHouse.co
-	function VerticalTimeline( element ) {
+	$('.sidenav').sidenav().on('click tap', 'li a', () => {
+		$('.sidenav').sidenav('close');
+	});
+	$(window).load(function () {
+		if ($(window).width() <= 600) {
+			$('.social').addClass('btn-small');
+		} else {
+			$('.social').removeClass('btn-small');
+		}
+		$(window).resize(function () {
+			if ($(window).width() <= 600) {
+				$('.social').addClass('btn-small');
+			} else {
+				$('.social').removeClass('btn-small');
+			}
+		});
+	});
+	$('.scrollspy').scrollSpy();
+	$('.collapsible').collapsible();
+	$('.tooltipped').tooltip();
+	$('.tap-target').tapTarget();
+
+	// Vertical Timeline - by CodyHouse.co
+	function VerticalTimeline(element) {
 		this.element = element;
 		this.blocks = this.element.getElementsByClassName("js-cd-block");
 		this.images = this.element.getElementsByClassName("js-cd-img");
@@ -31,30 +32,30 @@ $(function () {
 		this.hideBlocks();
 	};
 
-	VerticalTimeline.prototype.hideBlocks = function() {
+	VerticalTimeline.prototype.hideBlocks = function () {
 		//hide timeline blocks which are outside the viewport
-		if ( !"classList" in document.documentElement ) {
+		if (!"classList" in document.documentElement) {
 			return;
 		}
 		var self = this;
-		for( var i = 0; i < this.blocks.length; i++) {
-			(function(i){
-				if( self.blocks[i].getBoundingClientRect().top > window.innerHeight*self.offset ) {
-					self.images[i].classList.add("cd-is-hidden"); 
-					self.contents[i].classList.add("cd-is-hidden"); 
+		for (var i = 0; i < this.blocks.length; i++) {
+			(function (i) {
+				if (self.blocks[i].getBoundingClientRect().top > window.innerHeight * self.offset) {
+					self.images[i].classList.add("cd-is-hidden");
+					self.contents[i].classList.add("cd-is-hidden");
 				}
 			})(i);
 		}
 	};
 
-	VerticalTimeline.prototype.showBlocks = function() {
-		if ( ! "classList" in document.documentElement ) {
+	VerticalTimeline.prototype.showBlocks = function () {
+		if (! "classList" in document.documentElement) {
 			return;
 		}
 		var self = this;
-		for( var i = 0; i < this.blocks.length; i++) {
-			(function(i){
-				if( self.contents[i].classList.contains("cd-is-hidden") && self.blocks[i].getBoundingClientRect().top <= window.innerHeight*self.offset ) {
+		for (var i = 0; i < this.blocks.length; i++) {
+			(function (i) {
+				if (self.contents[i].classList.contains("cd-is-hidden") && self.blocks[i].getBoundingClientRect().top <= window.innerHeight * self.offset) {
 					// add bounce-in animation
 					self.images[i].classList.add("cd-timeline__img--bounce-in");
 					self.contents[i].classList.add("cd-timeline__content--bounce-in");
@@ -68,16 +69,16 @@ $(function () {
 	var verticalTimelines = document.getElementsByClassName("js-cd-timeline"),
 		verticalTimelinesArray = [],
 		scrolling = false;
-	if( verticalTimelines.length > 0 ) {
-		for( var i = 0; i < verticalTimelines.length; i++) {
-			(function(i){
+	if (verticalTimelines.length > 0) {
+		for (var i = 0; i < verticalTimelines.length; i++) {
+			(function (i) {
 				verticalTimelinesArray.push(new VerticalTimeline(verticalTimelines[i]));
 			})(i);
 		}
 
 		//show timeline blocks on scrolling
-		window.addEventListener("scroll", function(event) {
-			if( !scrolling ) {
+		window.addEventListener("scroll", function (event) {
+			if (!scrolling) {
 				scrolling = true;
 				(!window.requestAnimationFrame) ? setTimeout(checkTimelineScroll, 250) : window.requestAnimationFrame(checkTimelineScroll);
 			}
@@ -85,12 +86,38 @@ $(function () {
 	}
 
 	function checkTimelineScroll() {
-		verticalTimelinesArray.forEach(function(timeline){
+		verticalTimelinesArray.forEach(function (timeline) {
 			timeline.showBlocks();
 		});
 		scrolling = false;
 	};
-}); 
+
+	const scriptURL = 'https://script.google.com/macros/s/AKfycbyl0ET2gt1_5PsaeHdOO_ILgZxK5g11JQOmv-s3JesQbejq9wk/exec'
+	const form = document.forms['submit-to-google-sheet']
+
+	form.addEventListener('submit', e => {
+		e.preventDefault()
+		fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+			.then(response => console.log('Success!', response))
+			.catch(error => console.error('Error!', error.message))
+		form.className = 'close';
+		formComplete();
+	})
+
+
+	function formComplete() {
+		const h1 = document.createElement('h1');
+		h1.classList.add('end');
+
+		h1.appendChild(document.createTextNode(`Vaša prijava je zabeležena. Uskoro ćemo vas kontaktirati o daljem procesu selekcije!`));
+		setTimeout(() => {
+			form.parentElement.appendChild(h1);
+			setTimeout(() => h1.style.opacity = 1, 50);
+		}, 1000);
+	}
+
+
+});
 
 // end of document ready
 
